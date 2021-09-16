158ad34d932edc90a449f3a19ab9d37c018aec3f
richu101 <richubini101@gmail.com>
2021-09-17 04:14:28 +0900

    i2c master python code for arduino added

---
+++ I2c_Arduino_with_RPI/RPI_Master.py
@@ -0,0 +1,16 @@
+from smbus import SMBus
+
+addr = 0x8 # BUS assr
+bus = SMBus(1) # Select i2c one in rpi
+numb = 1
+
+print("Enter 1 to ON and 0 to OFF")
+
+while numb == 1:
+    ledstate = int(input(">>>   "))
+    if ledstate == 1:
+        bus.write_byte(addr, 0x1);
+    elif ledstate == 0:
+        bus.write_byte(addr, 0x0);
+    else:
+        numb = 0 
