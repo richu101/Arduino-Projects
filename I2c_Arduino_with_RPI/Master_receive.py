9e435c9136262491accc6c4a093cfe784f0c53c4
richu101 <richubini101@gmail.com>
2021-09-21 16:14:33 +0900

    master receive code added

--- I2c_Arduino_with_RPI/Master_receive.py
+++ I2c_Arduino_with_RPI/Master_receive.py
@@ -1,13 +1,27 @@
 
+import smbus
 import time
-from smbus import SMBus
+# for RPI version 1, use "bus = smbus.SMBus(0)"
+bus = smbus.SMBus(1)
 
-addr = 0x8 # BUS addr
-bus = SMBus(1) # Select i2c one in rpi
-numb = 1
+# This is the address we setup in the Arduino Program
+address = 0x08
 
+def writeNumber(value):
+    bus.write_byte(address, value)
+    # bus.write_byte_data(address, 0, value)
+    return -1
+
+def readNumber():
+    number = bus.read_byte(address)
+    # number = bus.read_byte_data(address, 1)
+    return number
 
 while True:
-
-    read = bus.read_byte(addr)
-    time.sleep(3) 
+    var = int(input(">>> "))
+    if not var:
+        continue
+    writeNumber(var)
+    number = readNumber()
+    print("received number = ",number)
+    
