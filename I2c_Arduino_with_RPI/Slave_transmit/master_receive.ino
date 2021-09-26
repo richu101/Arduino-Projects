b1f501713c124df226b4b03f9808ae6874a44c08
richu101 <richubini101@gmail.com>
2021-09-26 21:12:29 +0530

    ..

---
+++ I2c_Arduino_with_RPI/Slave_transmit/master_receive.ino
@@ -0,0 +1,20 @@
+#include<Wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);                        //Begins Serial Communication at 9600 baud rate
+  Wire.begin();     
+
+}
+
+void loop() {
+  Wire.requestFrom(8,1);
+  delay(10);
+   byte MasterReceive = Wire.read();
+   if(MasterReceive != 255){
+    Serial.println(MasterReceive);
+   }
+    
+   
+}
+
