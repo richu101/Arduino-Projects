d7a317415b0a4dfa0a0f4a78ff68d2366776d2df
richu101 <richubini101@gmail.com>
2021-10-11 10:13:46 +0530

    error comment is added

--- I2c_Arduino_with_RPI/Slave_transmit/master_receive.ino
+++ I2c_Arduino_with_RPI/Slave_transmit/master_receive.ino
@@ -10,7 +10,7 @@ void setup() {
 void loop() {
   Wire.requestFrom(8,1);
   delay(10);
-   byte MasterReceive = Wire.read();
+   byte MasterReceive = Wire.read(); // when clock is streched the wire.read function is returning 14
    if(MasterReceive != 255){ // TWI comminication in arduino is active low 
     Serial.println(MasterReceive);
    }
